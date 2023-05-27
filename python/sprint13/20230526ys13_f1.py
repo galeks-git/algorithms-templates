199be4485cff3e384ebc8a93589e58bcc53f5ad4
Galeks <1@1.ru>
2023-05-27 20:08:11 +0700

    sprint13 home 20230527

--- python/sprint13/20230526ys13_f1.py
+++ python/sprint13/20230526ys13_f1.py
@@ -75,6 +75,9 @@
 
 # 87465081
 
+# ------------------------------
+# 20230526 try1
+# ------------------------------
 
 # def binarySearch(arr, x, left, right) -> int:
 #     if right <= left: # промежуток пуст
@@ -215,7 +218,112 @@
 #     print(find_it_index)
 
 
+
+
+
+
+
+# ------------------------------
+# 20230526 try2
+# ------------------------------
+
+# def binarySearch(arr, x, left, right) -> int:
+#     if right <= left: # промежуток пуст
+#         return -1
+#     # промежуток не пуст
+#     mid = (left + right) // 2
+#     if arr[mid] == x: # центральный элемент — искомый
+#         return mid
+#     elif x < arr[mid]: # искомый элемент меньше центрального
+#                        # значит следует искать в левой половине
+#         return binarySearch(arr, x, left, mid)
+#     else: # иначе следует искать в правой половине
+#         return binarySearch(arr, x, mid + 1, right)
+
+# def broken_search(array, find_target) -> int:
+#     #  Your code
+#     #  “ヽ(´▽｀)ノ”
+#     # print('begin broken_search')
+#     # print("1 nums=", nums)
+#     # print("1 target=", target)
+#     # # len_nums=len(nums)
+#     # target_index = find_broken(nums, target)
+#     # return target_index
+
+# # def find_broken(array, find_target) -> int:
+# #     print('begin find_broken')
+
+#     if array[0]==find_target:
+#         return 0
+#     if array[-1]==find_target:
+#         return len(array)-1
+
+#     len_array= len(array)
+#     half_len_array= len_array // 2
+#     print("1 array=", array)
+#     print("1 len_array=", len_array)
+#     print("1 find_target=", find_target)
+#     print("1 half_len_array=", half_len_array)
+#     print("1 array[0]=", array[0])
+#     print("1 array[half_len_array]=", array[half_len_array])
+#     print("1 array[-1]=", array[-1])
+
+#     if len_array in [1,2,3,4]:
+#         print('len_array in [1,2,3,4]')
+#         for i in range(len_array):
+#             print("1234 find_target=", find_target)
+#             print("1234 array[i]=", array[i])
+#             print("1234 i=", i)
+#             if array[i]==find_target:
+#                 print("1234 if i=", i)
+#                 return i
+#         return -1
+
+
+#     if array[0] < array[-1]:
+#         if array[0] < find_target < array[-1]:
+#             find_target_index = binarySearch(array, find_target, 0, len_array)
+#             if find_target_index != -1:
+#                 return find_target_index
+#         else:
+#             return -1
+#     else:
+#         find_target_index=broken_search(array[0 : half_len_array], find_target)
+#         if find_target_index != -1:
+#             return find_target_index
+#         find_target_index=broken_search(array[half_len_array : len_array], find_target)
+#         if find_target_index != -1:
+#             find_target_index+=half_len_array
+#             return find_target_index
+#     return find_target_index
+
+
+
+# # def test():
+# #     arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
+# #     assert broken_search(arr, 5) == 6
+
+
+# if __name__ == '__main__':
+#     file = open("input.txt", "r")
+#     # В первой строке записано число n –— длина массива.
+#     array_len = int(file.readline())
+#     # Во второй строке записано положительное число k –— искомый элемент. 
+#     find_it = int(file.readline())
+#     # Далее в строку через пробел записано n натуральных чисел – элементы массива.
+#     input_array = tuple(map(int, file.readline().split()))
+#     file.close()
+#     print('begin')
+#     find_it_index = broken_search(input_array, find_it)
+#     print(find_it_index)
+
+
+# ------------------------------
+# 20230527 try1
+# ------------------------------
+
 def binarySearch(arr, x, left, right) -> int:
+    # print('begin binarySearch')
     if right <= left: # промежуток пуст
         return -1
     # промежуток не пуст
@@ -228,68 +336,85 @@ def binarySearch(arr, x, left, right) -> int:
     else: # иначе следует искать в правой половине
         return binarySearch(arr, x, mid + 1, right)
 
-def broken_search(array, find_target) -> int:
+
+# def find_broken(array, find_target) -> int:
+def find_broken(array, start_index, end_index, find_target) -> int:
+    # print('begin find_broken')
+
+    if array[start_index]==find_target:
+        return start_index
+    if array[end_index]==find_target:
+        return end_index
+
+    len_array= end_index-start_index+1
+    half_len_array= (len_array // 2)+start_index
+    # str_arr=[]
+    # for i in range(start_index, end_index+1):
+    #     str_arr.append(array[i])
+    # print("1 array=", str_arr)
+    # print("1 start_index=", start_index)
+    # print("1  end_index=", end_index)
+    # print("1        find_target=", find_target)
+    # print("1  half_len_array=", half_len_array)
+    # print("1   len_array=", len_array)
+    # print("1 array[start_index]=", array[start_index])
+    # print("1  array[half_len_array]=", array[half_len_array])
+    # print("1   array[end_index]=", array[end_index])
+
+    if len_array < 5:
+        # print('len_array in [1,2,3,4]')
+        for i in range(start_index, end_index+1):
+            # print("1234 find_target=", find_target)
+            # print("1234 array[i]=", array[i])
+            # print("1234 i=", i)
+            if array[i]==find_target:
+                # print("1234 if i=", i)
+                return i
+        return -1
+
+    if array[start_index] < array[end_index]:
+        # print('if array[start_index] < array[end_index]:')
+        if array[start_index] < find_target < array[end_index]:
+            # print('if array[start_index] << find_target <  array[end_index]:')
+            find_target_index = binarySearch(array, find_target, start_index, end_index)
+            if find_target_index != -1:
+                return find_target_index
+        else:
+            # print('else array[start_index] << find_target <  array[end_index]:')
+            return -1
+    else:
+        # print('2 left find_target_index begin')
+        # print("2 left    start_index=", start_index)
+        # print("2 left     half_len_array=", half_len_array)
+        find_target_index=find_broken(array,start_index,half_len_array-1, find_target)
+        # print('2 left find_target_index=', find_target_index)
+
+        if find_target_index != -1:
+            # print('3  end left find_target_index=', find_target_index)
+            return find_target_index
+
+        # print('2 right find_target_index begin')
+        # print("2 r    half_len_array=", half_len_array)
+        # print("2 r     end_index=", end_index)
+        find_target_index=find_broken(array,half_len_array,end_index-1, find_target)
+        # print('2 right find_target_index=', find_target_index)
+
+        if find_target_index != -1:
+            # print('3  end right find_target_index=', find_target_index)
+            return find_target_index
+
+    # print('4  end return find_target_index=', find_target_index)
+    return find_target_index
+
+
+def broken_search(nums, target) -> int:
     #  Your code
     #  “ヽ(´▽｀)ノ”
     # print('begin broken_search')
     # print("1 nums=", nums)
     # print("1 target=", target)
-    # # len_nums=len(nums)
-    # target_index = find_broken(nums, target)
-    # return target_index
-
-# def find_broken(array, find_target) -> int:
-#     print('begin find_broken')
-
-    if array[0]==find_target:
-        return 0
-    if array[-1]==find_target:
-        return len(array)-1
-
-    len_array= len(array)
-    half_len_array= len_array // 2
-    print("1 array=", array)
-    print("1 len_array=", len_array)
-    print("1 find_target=", find_target)
-    print("1 half_len_array=", half_len_array)
-    print("1 array[0]=", array[0])
-    print("1 array[half_len_array]=", array[half_len_array])
-    print("1 array[-1]=", array[-1])
-
-    if len_array in [1,2,3,4]:
-        print('len_array in [1,2,3,4]')
-        for i in range(len_array):
-            print("1234 find_target=", find_target)
-            print("1234 array[i]=", array[i])
-            print("1234 i=", i)
-            if array[i]==find_target:
-                print("1234 if i=", i)
-                return i
-        return -1
-
-
-    if array[0] < array[-1]:
-        if array[0] < find_target < array[-1]:
-            find_target_index = binarySearch(array, find_target, 0, len_array)
-            if find_target_index != -1:
-                return find_target_index
-        else:
-            return -1
-    else:
-        find_target_index=broken_search(array[0 : half_len_array], find_target)
-        if find_target_index != -1:
-            return find_target_index
-        find_target_index=broken_search(array[half_len_array : len_array], find_target)
-        if find_target_index != -1:
-            find_target_index+=half_len_array
-            return find_target_index
-    return find_target_index
-
-
-
-# def test():
-#     arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
-#     assert broken_search(arr, 5) == 6
+    target_index = find_broken(nums, 0, len(nums)-1, target)
+    return target_index
 
 
 if __name__ == '__main__':
@@ -301,6 +426,7 @@ if __name__ == '__main__':
     # Далее в строку через пробел записано n натуральных чисел – элементы массива.
     input_array = tuple(map(int, file.readline().split()))
     file.close()
-    print('begin')
+    # print('begin')
     find_it_index = broken_search(input_array, find_it)
     print(find_it_index)
+
--- python/sprint13/20230526ys13_f2.py
+++ python/sprint13/20230526ys13_f2.py
@@ -37,26 +37,189 @@
 # Для отсортированного списка участников выведите по порядку их логины по одному в строке.
 
 
+# ------------------------------
+# 20230527 try1
+# ------------------------------
+
+# if __name__ == '__main__':
+#     file = open("input.txt", "r")
+#     # В первой строке задано число участников n
+#     pipl_count = int(file.readline())
+#     #  В каждой из следующих n строк задана информация про одного из участников.
+#     # i-й участник описывается тремя параметрами:
+
+#     # уникальным логином (строкой из маленьких латинских букв длиной не более 20)
+#     # числом решённых задач Pi
+#     # штрафом Fi
+#     # Fi и Pi — целые числа, лежащие в диапазоне от 0 до 109.
+
+#     pipl_list = []
+#     for i in range(pipl_count):
+#         pipl_list.append(str(file.readline()).rstrip())
+#         # pipl_list.append(str(file.readline()).rstrip().split())
+#     print('pipl_count=', pipl_count)
+#     print('pipl_list=', pipl_list)
+#     file.close()    
+
+#     print('begin')
+
+
+# ------------------------------
+# 20230527 try1 pachka
+# https://app.pachca.com/chats/3834079?thread_id=1102945
+# ------------------------------
+
+# from dataclasses import dataclass
+# from typing import List, Tuple
+
+
+# # id = 87563686
+
+
+# @dataclass
+# class Competitor:
+#     name: str
+#     solved: int
+#     penalties: int
+
+#     def __str__(self) -> str:
+#         return self.name
+
+#     def __lt__(self, other):
+#         return (
+#             (self.solved, other.penalties, self.name)
+#             < (other.solved, self.penalties, other.name)
+#         )
+
+
+# def effective_quick_sort(
+#     competitors: List[Competitor], start: int = 0, end: int = None
+# ) -> int:
+#     if not end:
+#         end = len(competitors) - 1
+#     if start >= end:
+#         return -1
+#     left = start
+#     right = end
+#     pivot = competitors[(start + end) // 2]
+#     while left <= right:
+#         while competitors[left] < pivot:
+#             left += 1
+#         while competitors[right] > pivot:
+#             right -= 1
+#         if left <= right:
+#             competitors[left], competitors[right] = competitors[right], competitors[left]
+#             left += 1
+#             right -= 1
+#     effective_quick_sort(competitors, start, right)
+#     effective_quick_sort(competitors, left, end)
+
+
+# # def data_sort(competitor: Competitor) -> List[Tuple[int, int, str]]:
+# #     parsing_result = Competitor(*competitor)
+# #     return [
+# #         -int(parsing_result.solved),
+# #         int(parsing_result.penalties),
+# #         str(parsing_result.name)
+# #     ]
+
+
+# # def read_input() -> List[Competitor]:
+# #     quantity = int(input())
+# #     users = [Competitor(*input().split()) for _ in range(quantity)]
+# #     return users
+
+# def read_input() -> List[Competitor]:
+#     file = open("input.txt", "r")
+#     quantity = int(file.readline())
+#     users = [Competitor(*file.readline().split()) for _ in range(quantity)]
+#     print('quantity=', quantity)
+#     print('users=', users)
+#     return users
+
+
+# def main():
+#     users = read_input()
+#     effective_quick_sort(users)
+#     print(*users, sep='\n')
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+
+
+
+
+
+
+# ------------------------------
+# 20230527 try2 pachka
+# https://app.pachca.com/chats/3834079?thread_id=1121911
+# ------------------------------
+import random
+
+
+class Competitor:
+    def __init__(self, name, problems_solved, fine):
+        self.name = name
+        self.problems_solved = int(problems_solved)
+        self.fine = int(fine)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __lt__(self, other):
+        if self.problems_solved > other.problems_solved:
+            return True
+        if self.problems_solved == other.problems_solved:
+            if self.fine < other.fine:
+                return True
+            if self.fine == other.fine:
+                if self.name < other.name:
+                    return True
+        return False
+
+
+def in_place_partition(array, pivot):
+    left = 0
+    right = len(array) - 1
+    while left <= right:
+        if array[left] > pivot and (pivot > array[right] or pivot == array[right]):
+            array[left], array[right] = array[right], array[left]
+            left += 1
+            right -= 1
+        elif array[left] > pivot and (array[right] > pivot or array[right] == pivot):
+            right -= 1
+        elif (array[left] < pivot or array[left] == pivot) and array[right] < pivot:
+            left += 1
+        else:
+            right -= 1
+            left += 1
+    return array, left
+
+
+def optimised_quick_sort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = random.choice(array)
+        partition_result = in_place_partition(array, pivot)
+        left_part_end = partition_result[1]
+        left = partition_result[0][:left_part_end]
+        right = partition_result[0][left_part_end:]
+        return optimised_quick_sort(left) + optimised_quick_sort(right)
+
 
 if __name__ == '__main__':
     file = open("input.txt", "r")
-    # В первой строке задано число участников n
-    pipl_count = int(file.readline())
-    #  В каждой из следующих n строк задана информация про одного из участников.
-    # i-й участник описывается тремя параметрами:
-
-    # уникальным логином (строкой из маленьких латинских букв длиной не более 20)
-    # числом решённых задач Pi
-    # штрафом Fi
-    # Fi и Pi — целые числа, лежащие в диапазоне от 0 до 109.
-
-    pipl_list = []
-    for i in range(pipl_count):
-        pipl_list.append(str(file.readline()).rstrip())
-        # pipl_list.append(str(file.readline()).rstrip().split())
-    print('pipl_count=', pipl_count)
-    print('pipl_list=', pipl_list)
-    file.close()    
-
-    print('begin')
-
+    competitors = []
+    number_of_candidates = int(file.readline())
+    for _ in range(number_of_candidates):
+        competitors.append(Competitor(*file.readline().split()))
+    file.close()
+    # print('number_of_candidates=', number_of_candidates)
+    # print('competitors=', competitors)
+    for i in optimised_quick_sort(competitors):
+        print(i)
